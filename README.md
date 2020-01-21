8be08b0106cdcc3840d07f3bca13635054b5940c
GodAspire <alreinstadler@gmail.com>
2020-01-21 09:35:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-home-ga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-home-ga)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
