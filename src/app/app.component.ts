0163f95e65cd633e4ecb4fb09b3e2e92e1a7e715
GodAspire <alreinstadler@gmail.com>
2020-01-21 11:39:59 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('headerElem', {static: true}) headerElem: ElementRef;
+
+  headerOffset: number = 104;
   mobileNavigation: boolean = false;
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
 
   }
 }
