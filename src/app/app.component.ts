aaf5bfc63add52ac2e87760aa8ae3bd20208fd80
GodAspire <alreinstadler@gmail.com>
2020-01-23 10:35:09 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,10 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit, AfterViewInit {
-
-  @ViewChild('headerElem', {static: true}) headerElem: ElementRef;
-
-  headerOffset: number = 104;
   mobileNavigation: boolean = false;
 
   ngOnInit() {
-
+    
   }
 
   ngAfterViewInit() {
